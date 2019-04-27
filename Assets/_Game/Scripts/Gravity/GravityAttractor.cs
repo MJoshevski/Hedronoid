0aa9f11b47ec2038218b213057c22bacf51ddd05
Matej Joshevski <matej.joshevski@gmail.com>
2019-04-27 17:54:13 +0200

    Returning scripts to right places

---
+++ Assets/_Game/Scripts/Gravity/GravityAttractor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MDKShooter.Gravity
+{
+    public class GravityAttractor : MonoBehaviour
+    {
+        private float m_Gravity = -9.81f;
+
+        public void Attract(Rigidbody rb)
+        {
+            Transform m_body = rb.transform;
+
+            Vector3 m_YVector = new Vector3(0, transform.position.y, 0);
+            Vector3 m_AttractedBodyYVector = new Vector3(0, m_body.position.y, 0);
+
+            Vector3 gravityUp = (m_AttractedBodyYVector - m_YVector).normalized;
+            Vector3 bodyUp = m_body.up;
+
+            rb.AddForce(gravityUp * m_Gravity);
+
+            Quaternion bodyRotation = m_body.rotation;
+            Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, gravityUp) * bodyRotation;
+            bodyRotation = Quaternion.Slerp(bodyRotation, targetRotation, 50 * Time.deltaTime);
+            Debug.LogFormat("Body Rotation: {0} --- Target ROtation: {1}", bodyRotation.eulerAngles.ToString(), targetRotation.eulerAngles.ToString());
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Gravity/GravityBody.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MDKShooter.Gravity
+{
+    public class GravityBody : MonoBehaviour
+    {
+        public GravityAttractor m_GravityAttractor;
+
+        private Rigidbody m_RB;
+
+        private void Awake()
+        {
+            m_RB = GetComponent<Rigidbody>();
+
+            if (!m_RB) m_RB = GetComponentInChildren<Rigidbody>();
+            if (!m_RB) m_RB = GetComponentInParent<Rigidbody>();
+            if (!m_RB) Debug.LogError("No Rigidbody found on object!");
+        }
+
+        private void Start()
+        {
+            m_RB.useGravity = false;
+        }
+
+        private void Update()
+        {
+            m_GravityAttractor.Attract(m_RB);
+        }
+    }
+}
