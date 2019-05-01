7775a1c775a43fe1676f69b7a7bab0452d5d6ce8
Matej Joshevski <matej.joshevski@gmail.com>
2019-05-01 15:00:31 +0200

    Switched Slerp with RotateTowards for a more immediate rotation

--- Assets/_Game/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/_Game/Scripts/Camera/ThirdPersonCamera.cs
@@ -15,7 +15,7 @@ namespace MDKShooter
         // 			Camera
 
         [SerializeField] private float m_MoveSpeed = 1f;                      // How fast the rig will move to keep up with the target's position.
-        [Range(0f, 10f)] [SerializeField] private float m_TurnSpeed = 1.5f;   // How fast the rig will rotate from user input.
+        [Range(100f, 1000f)] [SerializeField] private float m_TurnSpeed = 500f;   // How fast the rig will rotate from user input.
         [SerializeField] private float m_TiltMax = 75f;                       // The maximum value of the x axis rotation of the pivot.
         [SerializeField] private float m_TiltMin = 45f;                       // The minimum value of the x axis rotation of the pivot.
 
@@ -45,7 +45,7 @@ namespace MDKShooter
 
             if (transform.up != gravityService.GravityUp)
             {
-                transform.rotation = Quaternion.Slerp(
+                transform.rotation = Quaternion.RotateTowards(
                     transform.rotation,
                     gravityService.GravityRotation,
                     m_TurnSpeed * Time.deltaTime);
