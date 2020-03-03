556a26866c85ae309e856fa8e9b78d921e40fc3f
Matej Joshevski <matejj@tabtale.com>
2020-03-03 13:37:17 +0100

    WallRun setup
    [WallRun] Setting up structure [WIP]

---
+++ Assets/_Game/State Scripts/Conditions/MonitorWallRun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hedronoid
+{
+    [CreateAssetMenu(menuName ="Conditions/Monitor Wall-Run")]
+    public class MonitorWallRun : Condition
+    {
+        public StateActions onTrueAction;
+
+        public override void InitCondition(PlayerStateManager state)
+        {
+            onTrueAction.Execute_Start(state);
+        }
+
+        public override bool CheckCondition(PlayerStateManager state)
+        {
+            bool isPressed = state.jumpPressed;
+     
+            if (isPressed)
+            {
+                onTrueAction.Execute(state);
+            } 
+
+            return isPressed;
+        }
+    }
+}
---
+++ Assets/_Game/State Scripts/State Actions/HandleWallRun.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Hedronoid
+{
+    [CreateAssetMenu(menuName ="Actions/State Actions/Handle Wall-Run")]
+    public class HandleWallRun : StateActions
+    {
+        [Header("DebugView")]
+        [SerializeField] float _runCounter = 0f;
+        [SerializeField] bool _wallRunning;
+
+        IGravityService gravityService;
+        WallRunVariables wallRunVariables;
+
+        public bool WallRunning { get { return _wallRunning; } private set { _wallRunning = value; } }
+
+        public override void Execute_Start(PlayerStateManager states)
+        {
+            gravityService = GravityService.Instance;
+            wallRunVariables = states.wallRunVariables;
+        }
+
+        public override void Execute(PlayerStateManager states)
+        {
+            if (WallRunning)
+            {
+                _runCounter -= Time.fixedDeltaTime;
+
+                if (_runCounter > 0f)
+                {
+                    //Applying negative gravity
+                    states.Rigidbody.ApplyForce(
+                        gravityService.GravityUp *
+                        gravityService.Gravity *
+                        wallRunVariables.GravityNegateMultiplier);
+                }
+            }
+        }
+
+        void CheckIfWallRunning(Collision collision)
+        {
+            for (int i = 0; i < collision.contactCount; i++)
+            {
+                var contact = collision.contacts[i];
+                var dot = Vector3.Dot(GravityService.Instance.GravityUp, contact.normal);
+                if (Mathf.Approximately(0f, dot))
+                {
+                    WallRunning = true;
+                }
+            }
+        }
+    }
+}
