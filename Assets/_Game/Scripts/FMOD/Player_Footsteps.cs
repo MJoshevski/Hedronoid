b597cf16b80b202ba5f7fdc6c262cf9c69b8c4da
Goran Petrovski <60354533+Goran-G0R4N@users.noreply.github.com>
2020-03-08 13:37:42 +0100

    Added C# script and footsteps on run animation.
    Added C# script and footsteps on run animation.

---
+++ Assets/_Game/Scripts/FMOD/Player_Footsteps.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Footsteps : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayRunEvent ()
+    {
+        FMOD.Studio.EventInstance Run = FMODUnity.RuntimeManager.CreateInstance("event:/Foley/Footsteps");
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(Run, transform, GetComponent<Rigidbody>());
+        Run.start();
+        Run.release();
+    }
+}
