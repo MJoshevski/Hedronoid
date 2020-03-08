25130bc637a07b2f9c086d6173f28c90c2ea2696
Goran Petrovski <60354533+Goran-G0R4N@users.noreply.github.com>
2020-03-08 13:56:21 +0100

    Changed "Player_Footsteps" script.
    Changed "Player_Footsteps" script.

--- Assets/_Game/Scripts/FMOD/Player_Footsteps.cs
+++ Assets/_Game/Scripts/FMOD/Player_Footsteps.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class Player_Footsteps : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
 
-    void PlayRunEvent ()
+    void PlayRunEvent (string EventPath)
     {
-        FMOD.Studio.EventInstance Run = FMODUnity.RuntimeManager.CreateInstance("event:/Foley/Footsteps");
+        FMOD.Studio.EventInstance Run = FMODUnity.RuntimeManager.CreateInstance(EventPath);
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(Run, transform, GetComponent<Rigidbody>());
         Run.start();
         Run.release();
