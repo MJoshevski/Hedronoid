3fcebc263b57f3a08312327ae8b4e58a4470734d
Matej Joshevski <matej.joshevski@gmail.com>
2020-06-23 16:28:04 +0200

    Fixed aiming accuracy and added a raycast gizmo draw
    [Test_Planet Gravity] Baked light and turned off auto generation
    [Shooting] Proper accuracy and gizmo ray for seeing the shooting vector

--- Assets/_Game/Scripts/State Scripts/State Actions/Shooting.cs
+++ Assets/_Game/Scripts/State Scripts/State Actions/Shooting.cs
@@ -11,6 +11,9 @@ namespace Hedronoid
         public float fireRate_Auto = 0.2f;
         public float fireRate_Shotgun = 2f;
         public float fireRate_Rail = 5f;
+        public float shootForce_Auto = 8000f;
+        public float shootForce_Shotgun = 5000f;
+        public float shootForce_Rail = 100000f;
 
         public GameObject bullets_Auto;
         public GameObject bullets_Shotgun;
@@ -30,14 +33,18 @@ namespace Hedronoid
                 states.camera.value.ScreenPointToRay(
                     new Vector3(Screen.width / 2, Screen.height / 2, 0));
 
-            Vector3 shootDirection = LookRay.direction;          
+            RaycastHit hit;
+            Physics.Raycast(LookRay, out hit, 10000f);
+
+            Gizmos.Line(bulletOrigin.value.position, hit.point, Color.yellow);
+            Vector3 shootDirection = hit.point - bulletOrigin.value.position;
 
             if (Input.GetButton("Fire1") && 
                 Time.realtimeSinceStartup - states.lastFired_Auto > fireRate_Auto)
             {
                 auto = Instantiate(bullets_Auto, bulletOrigin.value.position, Quaternion.identity);
                 rb_auto = auto.GetComponent<Rigidbody>();
-                rb_auto.AddForce(shootDirection * 8000f);
+                rb_auto.AddForce(shootDirection.normalized * shootForce_Auto);
                 states.lastFired_Auto = Time.realtimeSinceStartup;
             }
             else if (Input.GetButtonDown("Fire2") &&
@@ -45,7 +52,7 @@ namespace Hedronoid
             {
                 GameObject shot = Instantiate(bullets_Shotgun, bulletOrigin.value.position, Quaternion.identity);
                 Rigidbody rb_shot = shot.GetComponent<Rigidbody>();
-                rb_shot.AddForce(shootDirection * 50000f);
+                rb_shot.AddForce(shootDirection.normalized * shootForce_Shotgun);
                 states.lastFired_Shotgun = Time.realtimeSinceStartup;
             }
         }
