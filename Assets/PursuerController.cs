2ca8c5d3aea10d90ec38cbbfd63c928c5405b198
Matej Joshevski <matej.joshevski@gmail.com>
2020-08-15 16:02:25 +0200

    A*Pathfinding and Pathfinder 3D
    Removed A* Project, added Pathfinder with 3D heuristics

---
+++ Assets/PursuerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Pursuer))]
+
+
+public class PursuerController : MonoBehaviour
+{
+    // TheGrapIsReady() will be executed when the gameObject receives the
+    // "TheGraphIsReady" message
+    Pursuer thisPursuerInstance;
+    public Transform target;
+    Vector3 targetOldPos;
+    public DefenderController dCInstance;
+    public float targetLesionAreaRadius;
+    public float targetPathUpdateOffset;
+
+    public float circleRadius = 30;
+    private void Start()
+    {
+        thisPursuerInstance = gameObject.GetComponent<Pursuer>();
+    }
+
+    public void TheGraphIsReady()
+    {
+        targetOldPos = target.transform.position;
+        targetPathUpdateOffset = 8;
+        thisPursuerInstance.MoveTo(target.transform);
+    }
+
+    public void Update()
+    {
+        if (Vector3.Distance(targetOldPos, target.position) > targetPathUpdateOffset)
+        {
+            targetOldPos = target.position;
+            if (thisPursuerInstance.GetCurCondition() == "Movement")
+                thisPursuerInstance.RefinePath(target);
+            else
+                thisPursuerInstance.MoveTo(target, true);
+        }
+    }
+
+    public void EventTargetReached()
+    {
+        transform.position = RandomCirclePos();
+        thisPursuerInstance.MoveTo(target);
+    }
+    Vector3 RandomCirclePos()
+    {
+        float alpha = Random.Range(0f, 360f);
+        float radAlpha = (alpha * (float)Mathf.PI) / 180.0F;
+        float x = circleRadius * (float)Mathf.Cos(radAlpha);
+        float z = circleRadius * (float)Mathf.Sin(radAlpha);
+        return new Vector3(x, 0, z);
+    }
+}
