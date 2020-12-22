3851ae9e2aa7b27e5bb62dc7833382e204736104
Matej Joshevski <matej.joshevski@gmail.com>
2020-12-22 17:14:19 +0100

    Portals package added
    Started implementation WIP

---
+++ Assets/PlayerPortalTraveler.cs
@@ -0,0 +1,65 @@
+﻿using Hedronoid;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPortalTraveler : PortalTraveller
+{
+    Camera cam;
+    public Vector2 pitchMinMax = new Vector2(-89, 89);
+    public float rotationSmoothTime = 0.1f;
+
+    public float yaw;
+    public float pitch;
+    float smoothYaw;
+    float smoothPitch;
+
+    float yawSmoothV;
+    float pitchSmoothV;
+    float verticalVelocity;
+    Vector3 velocity;
+    Vector3 smoothV;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    void Start()
+    {
+        cam = Camera.main;
+        yaw = transform.eulerAngles.y;
+        pitch = cam.transform.localEulerAngles.x;
+        smoothYaw = yaw;
+        smoothPitch = pitch;
+    }
+
+    void Update()
+    {
+        float mX = InputManager.Instance.PlayerActions.Look.X;
+        float mY = InputManager.Instance.PlayerActions.Look.Y;
+
+        // Verrrrrry gross hack to stop camera swinging down at start
+        float mMag = Mathf.Sqrt(mX * mX + mY * mY);
+        if (mMag > 5)
+        {
+            mX = 0;
+            mY = 0;
+        }
+
+        yaw += mX * InputManager.Instance.MouseHorizontalSensitivity;
+        pitch -= mY * InputManager.Instance.MouseVerticalSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        smoothPitch = Mathf.SmoothDampAngle(smoothPitch, pitch, ref pitchSmoothV, rotationSmoothTime);
+        smoothYaw = Mathf.SmoothDampAngle(smoothYaw, yaw, ref yawSmoothV, rotationSmoothTime);
+    }
+
+    public override void Teleport(Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot)
+    {
+        transform.position = pos;
+        Vector3 eulerRot = rot.eulerAngles;
+        float delta = Mathf.DeltaAngle(smoothYaw, eulerRot.y);
+        yaw += delta;
+        smoothYaw += delta;
+        transform.eulerAngles = Vector3.up * smoothYaw;
+        velocity = toPortal.TransformVector(fromPortal.InverseTransformVector(velocity));
+        Physics.SyncTransforms();
+    }
+}
---
+++ Assets/PortalCulling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCulling : MonoBehaviour
+{
+    public List<Portal> portals = new List<Portal>();
+
+    void OnPreCull()
+    {
+        for (int i = 0; i < portals.Count; i++)
+        {
+            portals[i].PrePortalRender();
+        }
+        for (int i = 0; i < portals.Count; i++)
+        {
+            portals[i].Render();
+        }
+        for (int i = 0; i < portals.Count; i++)
+        {
+            portals[i].PostPortalRender();
+        }
+    }
+}
