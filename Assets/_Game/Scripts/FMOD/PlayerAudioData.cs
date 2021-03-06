69a5d2a0fab4e7392f47815f47f2c74484132b23
Matej Joshevski <matej.joshevski@gmail.com>
2021-03-06 18:24:47 +0100

    Scriptable obj PlayerAudioData interface for caching Event instance pathnames
    And some scene changes as well

---
+++ Assets/_Game/Scripts/FMOD/PlayerAudioData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Hedronoid/Audio/Player", fileName = "New Player Sheet")]
+public class PlayerAudioData : ScriptableObject
+{
+    [Header("Movement")]
+    [FMODUnity.EventRef]
+    public string footsteps = null;
+    [FMODUnity.EventRef]
+    public string jump = null;
+    [FMODUnity.EventRef]
+    public string doubleJump = null;
+    [FMODUnity.EventRef]
+    public string dash = null;
+    [FMODUnity.EventRef]
+    public string land = null;
+
+    [Header("Shooting")]
+    [FMODUnity.EventRef]
+    public string[] bulletPrimary = null;
+    [FMODUnity.EventRef]
+    public string bulletSecondary = null;
+}
