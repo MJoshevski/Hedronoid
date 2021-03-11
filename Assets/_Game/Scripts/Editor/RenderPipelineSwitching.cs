12d7a0b0dcc308513563b5d0cd143b7facf167ea
Matej Joshevski <matej.joshevski@gmail.com>
2021-03-11 15:31:00 +0100

    Added URP/Built-in RP switching capabilities
    Press  Alt + `  (tilde key) to switch between URP and Built-in RP

---
+++ Assets/_Game/Scripts/Editor/RenderPipelineSwitching.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public static class RenderPipelineSwitching
+{
+    private const string URP_ASSET_PATH = "Render Pipeline Assets/URP/UniversalRenderPipelineAsset";
+
+    [MenuItem("Hedronoid/Switch Render Pipelines &`")]
+    public static void SwitchRenderPipelines()
+    {
+        UniversalRenderPipelineAsset urpPipeline = Resources.Load(URP_ASSET_PATH) as UniversalRenderPipelineAsset;
+
+        if (GraphicsSettings.currentRenderPipeline == urpPipeline)
+            GraphicsSettings.renderPipelineAsset = null;
+        else if (GraphicsSettings.currentRenderPipeline != urpPipeline)
+            GraphicsSettings.renderPipelineAsset = urpPipeline;
+    }
+}
