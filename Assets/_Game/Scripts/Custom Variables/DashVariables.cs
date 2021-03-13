ac935226175ce76c7c9343aa973f9eecb6e49f14
Matej Joshevski <matej.joshevski@gmail.com>
2021-03-13 17:07:19 +0100

    Dash cooldown to 1 sec

--- Assets/_Game/Scripts/Custom Variables/DashVariables.cs
+++ Assets/_Game/Scripts/Custom Variables/DashVariables.cs
@@ -9,9 +9,8 @@ namespace Hedronoid
         public int MaxDashes = 1;
         public float MaxDistance = 30f;
         public float MaxTime = 2f;
-        public float DashCooldown = 3f;
+        public float DashCooldown = 1f;
         public int DashesMade = 0;
-        public bool ContinuousInput = false;
         public PhysicalForceSettings PhysicalForce;
 	}
 }
