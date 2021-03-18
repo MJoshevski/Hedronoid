cb60e44c0bed3076be8cf5a3ec1ad202769a4cdf
Matej Joshevski <matej.joshevski@gmail.com>
2021-03-18 18:31:08 +0100

    Updated cone to include ball at the end
    Still working on bull dash

--- Assets/_Game/Scripts/Enemies/BullSensor.cs
+++ Assets/_Game/Scripts/Enemies/BullSensor.cs
@@ -7,18 +7,19 @@ namespace Hedronoid.AI
     public class BullSensor : GruntSensor
     {
         [SerializeField]
-        protected float maxRadius = 10f;
-        [SerializeField]
         protected float maxDistance = 35f;
         [SerializeField]
         protected float coneAngle = 15f;
 
         private Physics physics;
-
+        private float coneRadius;
         public Transform GetTargetInsideCone(Vector3 direction)
         {
+            float angle = Mathf.Deg2Rad * coneAngle;
+            coneRadius = maxDistance * Mathf.Tan(angle);
+
             // First check if we have any players in range
-            RaycastHit[] players = physics.ConeCastNonAlloc(transform.position, maxRadius, direction, m_colliderBuffer.Length, maxDistance, coneAngle);
+            RaycastHit[] players = physics.ConeCastNonAlloc(transform.position, coneRadius, direction, m_colliderBuffer.Length, maxDistance, coneAngle);
 
             if (players.Length > 0)
             {
@@ -26,14 +27,6 @@ namespace Hedronoid.AI
                 {
                     return players[0].collider.gameObject.transform;
                 }
-                //if (GetModifiedAggroValue(m_colliderBuffer[0].gameObject) > GetModifiedAggroValue(m_colliderBuffer[1].gameObject))
-                //{
-                //    return m_colliderBuffer[0].transform;
-                //}
-                //else
-                //{
-                //    return m_colliderBuffer[1].transform;
-                //}
             }
             return null;
         }
@@ -47,6 +40,12 @@ namespace Hedronoid.AI
                 maxDistance,
                 coneAngle,
                 Color.yellow);
+
+            float angle = Mathf.Deg2Rad * coneAngle;
+            coneRadius = maxDistance * Mathf.Tan(angle / 2f);
+
+            Vector3 sphereLocation = transform.position + (transform.forward * maxDistance);
+            Popcron.Gizmos.Sphere(sphereLocation, coneRadius, Color.yellow);
         }
     }
 
