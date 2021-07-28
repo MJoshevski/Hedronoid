83652388317b13c16cf74712311b444fe7703512
Matej Joshevski <matej.joshevski@gmail.com>
2021-07-28 20:08:14 +0200

    Added FSM to seeker units (Homing Mines) - Need to configure states and transitions next Also some material conversions for the model of the homing mine

--- Assets/_Game/Scripts/AI/Enemies/Steering/Units/SeekUnit.cs
+++ Assets/_Game/Scripts/AI/Enemies/Steering/Units/SeekUnit.cs
@@ -1,11 +1,23 @@
 ﻿using Hedronoid;
 using Hedronoid.Core;
+using Hedronoid.HNDFSM;
+using System;
 using UnityEngine;
 
 namespace UnityMovementAI
 {
-    public class SeekUnit : HNDMonoBehaviour, IGameplaySceneContextInjector
+    public class SeekUnit : HNDFiniteStateMachine, IGameplaySceneContextInjector
     {
+        public enum EStates
+        {
+            GoToTarget,
+            AttackTarget,
+            ReturnToDefault,
+            DefaultMovement,
+
+            Highest
+        }
+
         public Transform target;
         public GameplaySceneContext GameplaySceneContext { get; set; }
         SteeringBasics steeringBasics;
@@ -14,17 +26,45 @@ namespace UnityMovementAI
         {
             base.Awake();
             this.Inject(gameObject);
+
+            CreateState(EStates.DefaultMovement, OnDefaultMovementUpdate, null, null);
+
+            CreateState(EStates.GoToTarget, OnGoToTargetUpdate, null, null);
+            CreateState(EStates.AttackTarget, OnAttackTargetUpdate, null, null);
+            CreateState(EStates.ReturnToDefault, OnReturnToDefaultUpdate, null, null);
+
+        }
+
+        public virtual void OnGoToTargetUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnAttackTargetUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnReturnToDefaultUpdate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void OnDefaultMovementUpdate()
+        {
+            throw new NotImplementedException();
         }
         protected override void Start()
         {
             base.Start();
 
-
             steeringBasics = GetComponent<SteeringBasics>();
         }
 
-        void FixedUpdate()
+        protected override void FixedUpdate()
         {
+            base.FixedUpdate();
+
             if (GameplaySceneContext.Player.cachedTransform == null) return;
 
             target = GameplaySceneContext.Player.cachedTransform;
